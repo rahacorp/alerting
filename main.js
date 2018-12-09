bc3a669f526af6e412436a4e57528624699c2048
alireza <alireza.ebrahimi2006@gmail.com>
2018-12-09 19:25:15 +0330

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        var a = 2;
+        console.log('Hello ds xwe', a);
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,10 @@
+class Startup {
+    public static main(): number {
+        var a = 2
+
+        console.log('Hello ds xwe', a);
+        return 0;
+    }
+}
+
+Startup.main();
