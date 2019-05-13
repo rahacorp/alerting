36ff2f346515932efe1d9df187253b939d620b49
alireza <alireza.ebrahimi2006@gmail.com>
2019-05-13 14:03:32 +0430

    recentAgents endpoint added

--- src/webapp/Stats.controller.ts
+++ src/webapp/Stats.controller.ts
@@ -8,6 +8,7 @@ const guard = expressPerm();
 // Assign router to the express.Router() instance
 const router: Router = Router();
 
+
 router.get('/riskyUsers', guard.check('aduser:read'), async (req: Request, res: Response) => {
 	try {
 		let session = ClientFactory.createClient("neo4j_session");
@@ -82,7 +83,63 @@ router.get('/latestLogs', guard.check('log:read'), async (req: Request, res: Res
 					  "date_histogram" : {
 						  "field" : "@timestamp",
 						  "min_doc_count" : 0,
-						  "interval": "1h"
+						  "interval": "1h",
+						  "extended_bounds" : {
+							"min" : "now-30h",
+							"max" : "now"
+						  }
+					  }
+				  }
+			  }
+		  }
+		console.log(body)
+		const response = await elasticClient.search({
+			index: 'winlogbeat-*',
+			size: 0,
+			body: body
+		})
+		console.log(response)
+		res.send(response.aggregations.histogram.buckets)
+	} catch (err) {
+		res.status(400).json({
+			success: false,
+			message: err.message
+		});
+	}
+})
+
+
+router.get('/recentAgents', guard.check('log:read'), async (req: Request, res: Response) => {
+	try {
+		let elasticClient = ClientFactory.createClient('elastic')
+		// req.query.field
+		// req.query.prefix
+		let body = {
+			"size": 0, 
+			"query": {
+			  "range": {
+				  "@timestamp": {
+					  "gte": "now-10d"
+				  }
+			  }
+		  },
+			"aggs" : {
+				  "histogram" : {
+					  "date_histogram" : {
+						  "field" : "@timestamp",
+						  "min_doc_count" : 0,
+						  "interval": "1d",
+						  "extended_bounds" : {
+							"min" : "now-10d",
+							"max" : "now"
+						  }
+					  },
+					  "aggs" : {
+						  "machine_count" : {
+							  "cardinality": {
+								  "field" : "computer_name.keyword"
+							  }
+						  }
 					  }
 				  }
 			  }
