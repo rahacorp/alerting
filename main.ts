482c60f63f1014fd8cac22047c8416fe34831868
alireza <alireza.ebrahimi2006@gmail.com>
2019-05-28 22:45:17 +0430

    aothorziation header allow cors

--- main.ts
+++ main.ts
@@ -344,7 +344,7 @@ if (process.argv.length < 3) {
 	//enable cors 
 	app.use(function(req, res, next) {
 		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 		next();
 	});
 
